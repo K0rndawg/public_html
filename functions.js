372411273a2d5345acac0ac4faa6d0a71f9dc676
korndog3 <122747681+korndog3@users.noreply.github.com>
2023-09-14 20:22:28 -0500

    Update to style2

--- functions.js
+++ functions.js
@@ -7,15 +7,13 @@ function toggleStyleSheet(){
     var curr_sheet= styleSheet.getAttribute("href");
     // 1 (c) Determine new stylesheet file name
     // 1 (d) replace stylesheet with new stylesheet (hint: element.setAttribute)
-    if (!curr_sheet.endsWith("2.css")){
-        curr_sheet = curr_sheet.replace(".", "2.");
-        styleSheet.setAttribute("href", curr_sheet);
+    if (curr_sheet != "style2.css"){
+        curr_sheet = "style2.css";
     }
     else{
-        curr_sheet = curr_sheet.replace("2.", ".");
-        styleSheet.setAttribute("href", curr_sheet);
+        curr_sheet = "style.css";
     }
-
+    styleSheet.setAttribute("href", curr_sheet);
     // TASK 2
     // 2 (d) For persistence when page is refreshed. save new stylesheet name to localStorage
     // hint: localStorage.setItem(name, value)
@@ -24,13 +22,13 @@ function toggleStyleSheet(){
 }
 
 window.onload = function(){
-    var styleSheet = document.getElementById("mainStyleSheet");
     
     // 2 (a) Get stylesheet name from local storage hint: localStorage.getItem(name)
     var curr_sheet = localStorage.getItem("style_ref");
-
-    if (curr_sheet !== null) {
+    var styleSheet= document.getElementById("mainStyleSheet");
+    if (curr_sheet == null) {
         // 2 (c) Replace href attribute of html element.
-        styleSheet.setAttribute("href", curr_sheet);
+        styleSheet.setAttribute("href", "style.css");
     }
+    styleSheet.setAttribute("href", curr_sheet);
 }
